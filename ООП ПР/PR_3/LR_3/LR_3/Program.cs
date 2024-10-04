d31dbbdceb1bade3b9cd8c45c7eb87b9b7880cc5
Vasja1142 <122717158+Vasja1142@users.noreply.github.com>
2024-10-04 09:54:53 +0500

    save

---
+++ ООП ПР/PR_3/LR_3/LR_3/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+
+int length = 10;
+double y;
+double x;
+Console.WriteLine("y = cos(x)\n");
+for (int i = 1; i <= length; i++)
+{
+    x = (double)i / 10;
+    y = Math.Cos(x);
+    
+    Console.WriteLine($"X = {x}; Y = {y}");
+}
+
+Console.WriteLine("\nВведите x: ");
+bool f = double.TryParse(Console.ReadLine(), out x);
+
+while (!f)
+{
+    Console.WriteLine("Неверное значение!");
+    Console.WriteLine("Введите x: ");
+    f = double.TryParse(Console.ReadLine(), out x);
+}
+
+double a = 1;
+double b = 1;
+double S = 1;
+Console.WriteLine($"При n = 0; S = {S}");
+
+for (int n = 1; n <= length; n++)
+{
+    a *= x*x*(-1);
+    b *= (n + 1);
+    S = a / b;
+    Console.WriteLine($"При n = {n}; S = {S}");
+}
+
+
